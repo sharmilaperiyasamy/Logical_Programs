6d0f166497f569d2c787b96c00d1dd023ab2a652
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-07 12:34:25 +0530

    [Sharmila] perfect number

---
+++ Logical_Programs/Perfect_Number.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs
+{
+    internal class Perfect_Number
+    {
+        public void Perfect()
+        {
+            Console.WriteLine("Enter the number to check it is a perfect number or not : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine("The Divisors are " + i);
+                    sum = sum + i;
+                    if (sum != n)
+                    {
+                        Console.WriteLine("sum : " + sum);
+                    }
+                }
+            }
+            if (sum == n)
+            {
+                Console.WriteLine(n + " is a Perfect number");
+            }
+            else
+            {
+                Console.WriteLine(n + " is not a Perfect number");
+            }
+        }
+    }
+}
--- Logical_Programs/Program.cs
+++ Logical_Programs/Program.cs
@@ -1,2 +1,5 @@
 ﻿Logical_Programs.Fibonacci_Series fibonacci = new Logical_Programs.Fibonacci_Series();
 fibonacci.Series();
+
+Logical_Programs.Perfect_Number Per = new Logical_Programs.Perfect_Number();
+Per.Perfect();
